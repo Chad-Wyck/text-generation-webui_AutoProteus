eb515202b4dc137b07899e88bbb6e1ae8899dc1e
Chad-Wyck <93878363+Chad-Wyck@users.noreply.github.com>
2024-04-01 20:46:22 -0600

    settings.yaml update to proper default

---
+++ AP_DockerUbuntu_boot.bat
@@ -0,0 +1,1 @@
+docker run -itd --privileged --hostname=78aa085991cf --mac-address=02:42:ac:11:00:02 --env=PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin --volume=D:\AI\AutoProteus\Docker:/AutoProteus --name ubuntu --restart=no --label='org.opencontainers.image.ref.name=ubuntu' --label='org.opencontainers.image.version=22.04' --runtime=runc --memory="2147483648" -t -d ubuntu
