2029ec870f16fb985b48145b7973afe563318c6d
Chad-Wyck <93878363+Chad-Wyck@users.noreply.github.com>
2024-06-06 17:40:50 -0600

    Updated AutoProteus.bat to 24.04 Ubuntu

--- AutoProteus.bat
+++ AutoProteus.bat
@@ -1,3 +1,3 @@
-docker run -itd --privileged --hostname=78aa085991cf --mac-address=02:42:ac:11:00:02 --env=PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin --volume=C:\AutoProteus\Docker:/AutoProteus --name ubuntu --restart=no --label='org.opencontainers.image.ref.name=ubuntu' --label='org.opencontainers.image.version=20.04' --runtime=runc --memory="8147483648" -t -d ubuntu:20.04 /bin/bash -c "apt-get update && apt-get install -y sudo && exec bash"
+docker run -itd --privileged --hostname=78aa085991cf --mac-address=02:42:ac:11:00:02 --env=PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin --volume=C:\AutoProteus\Docker:/AutoProteus --name ubuntu --restart=no --label='org.opencontainers.image.ref.name=ubuntu' --label='org.opencontainers.image.version=24.04' --runtime=runc --memory="8147483648" -t -d ubuntu:24.04 /bin/bash -c "apt-get update && apt-get install -y sudo && exec bash"
 
 start_windows.bat --settings settings.yaml --max_seq_len 8096 --api 
